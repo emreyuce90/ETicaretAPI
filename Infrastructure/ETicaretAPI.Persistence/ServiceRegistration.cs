c9add4113a3d68dd9f0f45f47ee7750986e157e1
Emre YÜCE <emreyuce90@hotmail.com>
2022-09-06 17:00:41 +0300

    * Onion Arch created

--- Infrastructure/ETicaretAPI.Persistence/ServiceRegistration.cs
+++ Infrastructure/ETicaretAPI.Persistence/ServiceRegistration.cs
@@ -1,19 +1,17 @@
-﻿using ETicaretAPI.Application.Abstractions;
-using ETicaretAPI.Persistence.Concretes;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ETicaretAPI.Persistence.ServiceRegistration
+namespace ETicaretAPI.Persistence
 {
     public static class ServiceRegistration
     {
         public static void AddPersistentService(this IServiceCollection services)
         {
-            services.AddScoped<IProductService, ProductService>();
+            
         }
     }
 }
