a33cfb003c0824e6bec4fc534354bb04992de4c0
Emre YÜCE <emreyuce90@hotmail.com>
2022-09-06 12:07:09 +0300

    *Onion archt. tested with dummy datas

---
+++ Infrastructure/ETicaretAPI.Persistence/IoC/ServiceRegistration.cs
@@ -0,0 +1,19 @@
+﻿using ETicaretAPI.Application.Abstractions;
+using ETicaretAPI.Persistence.Concretes;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Persistence.ServiceRegistration
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistentService(this IServiceCollection services)
+        {
+            services.AddScoped<IProductService, ProductService>();
+        }
+    }
+}
--- Presentation/ETicaretAPI.API/Program.cs
+++ Presentation/ETicaretAPI.API/Program.cs
@@ -1,7 +1,9 @@
+using ETicaretAPI.Persistence.ServiceRegistration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddPersistentService();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
