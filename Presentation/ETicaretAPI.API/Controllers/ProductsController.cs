b1d25b74e7c2bfdd42dda414f1b20677e6ec0eb5
emreyuce90@hotmail.com <emreyuce90@hotmail.com>
2022-09-19 23:51:16 +0300

    *

--- Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
+++ Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
@@ -13,7 +13,7 @@ public class ProductsController : ControllerBase
 {
     private readonly IProductReadRepository _productReadRepository;
     private readonly IProductWriteRepository _productWriteRepository;
-
+    
     public ProductsController(IProductReadRepository productReadRepository, IProductWriteRepository productWriteRepository)
     {
         _productReadRepository = productReadRepository;
